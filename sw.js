6a56a3a367f234f69ea36ec3ad2dcc30015ef018
hamza-7-hussien <hamzahussien2357@gmail.com>
2026-08-29 17:17:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,63 +1,7 @@
-// sw.js - Service Worker for Spy Station
-const CACHE_NAME = 'spy-station-v1';
-const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    'https://i.imgur.com/8mYpR2N.png',
-    'https://i.imgur.com/4xVq3wQ.png',
-    'https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js',
-    'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth-compat.js',
-    'https://www.gstatic.com/firebasejs/9.6.10/firebase-database-compat.js'
-];
-
-// تثبيت Service Worker
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Cache opened');
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-// تفعيل Service Worker
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// معالجة الطلبات
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request).then(
-                    response => {
-                        if(!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-                        const responseToCache = response.clone();
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
-                        return response;
-                    }
-                );
-            })
-    );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
